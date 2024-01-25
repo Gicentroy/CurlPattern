43b433100bfdb0b308964c4230b306351c23af9b
Gicentroy Henry III <gicentroyhenry3@gmail.com>
2024-01-25 10:42:51 -0500

    Initial Commit

---
+++ CurlPattern/CurlPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurlPatternApp.swift
+//  CurlPattern
+//
+//  Created by Gicentroy Henry III on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CurlPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
