adf6abad539c3a8b2b532f4d648086721027db10
Ian Simons <itsimons122203@gmail.com>
2024-01-26 11:24:24 -0500

    Start to multiple choice questions

--- CurlPattern/ContentView.swift
+++ CurlPattern/ContentView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+struct QuestionView: View {
+    
+    @State var question = QuestionModel(question: "How often do you wash your hair", options: ["1 week", "1 day", "twice a week"])
+    
+    var body: some View {
+        Text(question.question)
+        ForEach(question.options, id: \.self) { option in
+            Button(option) {
+                question.select(option: option )
+            }
+        }
+        Text(question.selectedOptions ?? "no option selected")
+    }
+}
+
+struct QuestionModel {
+    var question: String
+    var options: [String]
+    var selectedOptions: String?
+    
+    mutating func select(option: String) {
+        self.selectedOptions = option
+    }
+}
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -20,5 +44,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    QuestionView()
 }
