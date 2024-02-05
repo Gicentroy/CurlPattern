018e3a2a60adf3509a0a4e66a15650d6d913348c
Gicentroy Henry III <gicentroyhenry3@gmail.com>
2024-02-05 11:08:04 -0500

    Interface Update Feb5

--- CurlPattern/ContentView.swift
+++ CurlPattern/ContentView.swift
@@ -14,34 +14,61 @@ struct QuestionView: View {
     @State var done = false
     
     var body: some View {
+        ZStack{
+            Image("Background")
+            VStack{
+                Text("Hair Type Survey!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                //.lineLimit(1)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("2F1000")/*@END_MENU_TOKEN@*/)
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/)
         // unwrap the optional
-        if let selectedQuestion = questionsViewModel.selectedQuestion {
+                if let selectedQuestion = questionsViewModel.selectedQuestion {
             VStack{
                 if done{
                     ResultsView()
                 }else{
+                    Spacer()
+                        .frame(width: 1.0, height: 260.469)
                     Text(selectedQuestion.question)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .bottom, .trailing], 30.0)
+                        .font(.system(size: 19))
                     ForEach(selectedQuestion.options, id: \.self) { option in
                         //                    Button(option) {
                         // assign the option to the `selectedOption` of our `selectedQuestion`
-                        
                         // call the `update` function for our questionsViewModel and pass in this `selectedQuestion`
                         Button {
                             if questionsViewModel.index < questionsViewModel.questions.count - 1{
                                 questionsViewModel.update()
-                            }else{
+                            }else {
                                 done = true
                             }
-                       
-                            
                         } label: {
                             Text(option)
-                        }
+                                .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color("2F1000"), lineWidth: 0)
+                                            //.fill(/*@START_MENU_TOKEN@*/Color("2F1000")/*@END_MENU_TOKEN@*/
+                                            )
+                                .padding(.all)
+                                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("2F1000")/*@END_MENU_TOKEN@*/)
+                                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/3/*@END_MENU_TOKEN@*/)
                     }
-                    
+                        Spacer()
+                            .frame(width: 1.0, height: 33.463)
+                    }
                 }
             }
         }
+                   }
+    }
     }
 }
 
@@ -66,7 +93,7 @@ struct ContentView: View {
 
 struct ResultsView: View {
     var body: some View {
-        Text("Results")
+        Text("Results") .foregroundColor(.white)
     }
 }
 
